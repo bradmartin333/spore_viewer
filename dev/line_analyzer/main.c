4e105a66ed6ab0af133e5bb6654c52cfc48f92e5
brad laptop <bradmartin333@gmail.com>
2025-03-19 21:52:28 -0600

    draw lines on image

--- dev/line_analyzer/main.c
+++ dev/line_analyzer/main.c
@@ -1,15 +1,60 @@
+#include <iostream>
+
 #include "raylib.h"
+
+bool is_between(int val, int min, int max) {
+    return val >= min && val <= max;
+}
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 631 + 150;
+    const int screenHeight = 509 + 150;
     InitWindow(screenWidth, screenHeight, "line analyzer");
-    while (!WindowShouldClose())
+    
+    Image usaf_target = LoadImage("../resources/usaf_target.png");
+    Rectangle usaf_rect = {0, 0, 1.0f * usaf_target.width, 1.0f * usaf_target.height};
+    Texture2D texture = LoadTextureFromImage(usaf_target);
+    UnloadImage(usaf_target);
+
+    bool test_toggle = false;
+    Vector2 a_pos = {-1, -1};
+    Vector2 b_pos = {-1, -1};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) 
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        
+        DrawTexture(texture, 0, 0, WHITE);
+
+        Vector2 mouse_pos = {-1, -1};
+        if (IsMouseButtonPressed(0)) {
+            Vector2 buffer_mouse_pos = GetMousePosition();
+            if (is_between(buffer_mouse_pos.x, 0, usaf_target.width) &&
+                is_between(buffer_mouse_pos.y, 0, usaf_target.height))
+                mouse_pos = buffer_mouse_pos;
+        }
+        
+        if (mouse_pos.x != -1 && mouse_pos.y != -1) {
+            test_toggle = !test_toggle;
+
+            if (test_toggle)
+                a_pos = mouse_pos;
+            else
+                b_pos = mouse_pos;
+        }
+
+        if (CheckCollisionPointRec(a_pos, usaf_rect) && 
+            CheckCollisionPointRec(b_pos, usaf_rect))
+            DrawLineEx(a_pos, b_pos, 3, GRAY);
+        DrawCircleV(a_pos, 5, GREEN);
+        DrawCircleV(b_pos, 5, RED);
+        
         EndDrawing();
     }
-    CloseWindow();
+    UnloadTexture(texture);
+    CloseWindow(); 
     return 0;
 }
--- dev/line_analyzer/run.sh
+++ dev/line_analyzer/run.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+set -e
+
 g++ main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o app
+
 ./app
+
